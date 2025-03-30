79266b07711ec72bfec849f932f8a1a117ab227f
edith.teodocia@gmail.com <edith.teodocia@gmail.com>
2025-03-30 00:11:31 -0400

    feat:menu de la landing page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
- 
+const body = document.body.firstChild;
+console.log(body);
+
+const menu = document.getElementById('menu_btn')
+const enlaces = document.getElementById('nav_links')
+const menuIcono = menu.querySelector('i')
+
+menu.addEventListener('click', (e)=>{
+    enlaces.classList.toggle('open');
+    const isopen = enlaces.classList.contains('open');
+    menuIcono.setAttribute("class", isopen? "ri-close-line":"ri-menu-line");
+});
+enlaces.addEventListener('click', (e)=>{
+    enlaces.classList.remove('open');
+     
+    menuIcono.setAttribute("class", "ri-menu-line");
+});
